35cd85b15269c5cdb7e6fa74587503957872ee9a
sack-of-pigs <jtk.dethparty@gmail.com>
2026-02-03 13:59:48 +0400

    Initial Commit

---
+++ src/main/java/ru/netology/services/VacationService.java
@@ -0,0 +1,29 @@
+package ru.netology.services;
+
+public class VacationService {
+    public int calculateVacationMonths(int income, int expenses, int threshold) {
+        int balance = 0;          // текущий баланс денег
+        int vacationMonths = 0;   // количество месяцев отдыха
+
+        for (int month = 1; month <= 12; month++) {
+            if (balance >= threshold) {
+                // Решение отдохнуть
+                int initialExpense = expenses;
+                int restExpense = (balance - expenses) * 2 / 3; // тратим 2/3 остатка после обязательных трат
+                balance -= expenses;
+                balance -= restExpense;
+
+                System.out.println("Месяц " + month + ". Денег " + balance + ". Буду отдыхать. Потратил -" + initialExpense + ", затем ещё -" + restExpense);
+
+                vacationMonths++;
+            } else {
+                // Решение работать
+                balance += income;
+                balance -= expenses;
+
+                System.out.println("Месяц " + month + ". Денег " + balance + ". Придётся работать. Заработал +" + income + ", потратил -" + expenses);
+            }
+        }
+        return vacationMonths;
+    }
+}
